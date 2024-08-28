a1faf7828dc83ff16842df60d986c1d9adc316f7
Latokha-Sofiia <132939164+Latokha-Sofiia@users.noreply.github.com>
2024-08-28 15:56:03 +0700

    MongoDB

--- backend/models/todo.model.js
+++ backend/models/todo.model.js
@@ -6,6 +6,6 @@ const todoSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 })
 
-// const Todo = mongoose.model("Todo", todoSchema)
+const Todo = mongoose.model("Todo", todoSchema)
 
 module.exports = Todo
